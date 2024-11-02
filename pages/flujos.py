8adb712fdcd7ce3d89dbe7dc877efdb922a8b19c
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 21:15:16 +0100

    Update flujos.py

--- pages/flujos.py
+++ pages/flujos.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 from utils import obtener_coordenadas as obcoor, pronostico as prn
 
-def mostrar_pagina():
+def show():
     st.header("Modelo predictivo de flujos")
     # Aquí incluirías todo el código específico para la predicción
