c082ab19240b2fe94574746ea71638e9efcb42f2
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 17:03:56 +0100

    Create inicio.py

---
+++ inicio.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def mostrar_pagina():
+    st.markdown("<h3 style='text-align: center;'>Universidade de Santiago de Compostela</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Grao en Intelixencia Artificial</h3>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center;'>Proxecto Integrador I</h4>", unsafe_allow_html=True)
+    
+    st.write("Este proyecto se encuadra dentro del caso de Uso...")
+    st.write("Se ha desarrollado esta aplicación web que...")
