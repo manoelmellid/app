e0e56af20e90b40b1599230f1f506c7289cadd96
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 20:44:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from pages import inicio, prediccion, navegacion, flujos, ocupacion
 col1, col2, col3 = st.columns([3,3,3])
 with col1:
     st.image("amtega_logo.png_2089811488.png", use_column_width=True)
+with col3:
+    st.header("SMETRIA")
 
 st.sidebar.header("Menú de navegación")
 pagina = st.sidebar.radio(
