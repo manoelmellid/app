a4bb7151564bc5aead9bc4bc081e95931f8aebbf
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 17:57:40 +0100

    Update inicio.py

--- pages/inicio.py
+++ pages/inicio.py
@@ -5,5 +5,5 @@ def mostrar_pagina():
     st.markdown("<h3 style='text-align: center;'>Grao en Intelixencia Artificial</h3>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center;'>Proxecto Integrador I</h4>", unsafe_allow_html=True)
     
-    st.write("Este proyecto se encuadra dentro del caso de Uso...")
-    st.write("Se ha desarrollado esta aplicación web que...")
+    st.write("Este proyecto se encuadra dentro del caso de Uso \"Sistema de monitorización de eventos en los últimos tramos del Camino de Santiago\".")
+    st.write("Se ha desarrollado esta aplicación web que agrupa todas las funcionalidades requeridas por el caso de uso planteado por la entidad colaboradora AMTEGA.")
