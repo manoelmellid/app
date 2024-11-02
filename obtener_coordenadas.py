f1febadb71faf2a2a99d811cb468706728ce89c1
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 14:28:54 +0100

    Update obtener_coordenadas.py

--- obtener_coordenadas.py
+++ obtener_coordenadas.py
@@ -7,11 +7,14 @@ def query_csv_data(km_value):
     # Filtrar los datos donde la columna 'km' es igual a km_value
     filtered_df = df[df['km'] == km_value][['longitud', 'latitud', 'concello_id', 'ubicacion']]
     
-    # Extraer las columnas como listas
-    longitud = filtered_df['longitud'].tolist()
-    latitud = filtered_df['latitud'].tolist()
-    concello_id = filtered_df['concello_id'].tolist()
-    ubicacion = filtered_df['ubicacion'].tolist()
-
-    # Retornar las listas
-    return longitud, latitud, concello_id, ubicacion
+    # Verificar si el DataFrame filtrado no está vacío
+    if not filtered_df.empty:
+        # Retornar el primer elemento de cada columna como flotante, excepto concello_id y ubicacion
+        longitud = float(filtered_df['longitud'].iloc[0])  # Convertir a float
+        latitud = float(filtered_df['latitud'].iloc[0])    # Convertir a float
+        concello_id = filtered_df['concello_id'].iloc[0]   # Texto
+        ubicacion = filtered_df['ubicacion'].iloc[0]       # Texto
+        return longitud, latitud, concello_id, ubicacion
+    else:
+        # Si no se encuentra el km_value, retornar None para los valores de longitud y latitud
+        return None, None, None, None
