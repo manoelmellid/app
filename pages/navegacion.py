b0e78d31361dbe1958bd366f04d16af637e635bf
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 17:16:02 +0100

    Create navegacion.py

---
+++ pages/navegacion.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from utils import obtener_coordenadas as obcoor, pronostico as prn
+
+def mostrar_pagina():
+    st.header("Predicción meteorológica")
+    # Aquí incluirías todo el código específico para la predicción
