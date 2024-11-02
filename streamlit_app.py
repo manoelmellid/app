bf60b4bbc27e32dc30c41c41b66751bb07191d92
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 16:06:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ col1, col2, col3 = st.columns([3,3,3])
 with col1:
     st.image("amtega_logo.png_2089811488.png", use_column_width=True)
 
-st.markdown("<h1 style='text-align: center;'>SMETRIAo</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>SMETRIA</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>Sistema de eventos en tiempo real</h2>", unsafe_allow_html=True)
 
 st.sidebar.header("Menú de navegación")
