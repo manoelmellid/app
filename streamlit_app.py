c8a070af1c290320938b92eba9428bd9eb7747f6
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 17:48:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from pages import prediccion, navegacion, flujos, ocupacion
 
+col1, col2, col3 = st.columns([3,3,3])
+with col1:
+    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
+
 st.sidebar.header("Menú de navegación")
 pagina = st.sidebar.radio(
     "", ["Inicio", "Predicción meteorológica", "Herramienta de navegación (BETA)", 
@@ -11,8 +15,8 @@ st.markdown("<h3 style='text-align: center;'>Universidade de Santiago de Compost
 st.markdown("<h3 style='text-align: center;'>Grao en Intelixencia Artificial</h3>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center;'>Proxecto Integrador I</h4>", unsafe_allow_html=True)
 
-st.write("Este proyecto se encuadra dentro del caso de Uso...")
-st.write("Se ha desarrollado esta aplicación web que...")
+st.write("Este proyecto se encuadra dentro del caso de Uso \"Sistema de monitorización de eventos en los últimos tramos del Camino de Santiago\".")
+st.write("Se ha desarrollado esta aplicación web que agrupa todas las funcionalidades requeridas por el caso de uso planteado por la entidad colaboradora AMTEGA.")
 
 if pagina == "Predicción meteorológica":
     prediccion.mostrar_pagina()
