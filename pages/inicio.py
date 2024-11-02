0aad41bd5730cc019a24699c0a21cb2d9778e92f
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 20:54:44 +0100

    Update inicio.py

--- pages/inicio.py
+++ pages/inicio.py
@@ -9,6 +9,26 @@ def show():
     st.write("Este proyecto se encuadra dentro del caso de Uso \"Sistema de monitorización de eventos en los últimos tramos del Camino de Santiago\".")
     st.write("Se ha desarrollado esta aplicación web que agrupa todas las funcionalidades requeridas por el caso de uso planteado por la entidad colaboradora AMTEGA.")
 
+    st.markdown(
+    """
+    <style>
+    .stButton>button {
+        color: white; /* Color del texto */
+        background-color: #4CAF50; /* Color de fondo del botón */
+        padding: 10px 20px; /* Espaciado interno */
+        border: none; /* Sin borde */
+        border-radius: 5px; /* Bordes redondeados */
+        font-size: 16px; /* Tamaño de la fuente */
+        cursor: pointer; /* Cursor de puntero */
+    }
+    .stButton>button:hover {
+        background-color: #45a049; /* Color de fondo al pasar el mouse */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+    
     col1, col2, col3, col4 = st.columns(4)
     
     # Colocar los botones en las columnas
