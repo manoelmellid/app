0ef65e6de20318f819dc9e9d9192e802e1d1d119
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-02 17:21:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import inicio, prediccion, navegacion, flujos, ocupacion
+from pages import prediccion, navegacion, flujos, ocupacion
 
 st.sidebar.header("Menú de navegación")
 pagina = st.sidebar.radio(
@@ -7,9 +7,14 @@ pagina = st.sidebar.radio(
          "Modelo predictivo de flujos (BETA)", "Modelo predictivo de ocupación (BETA)"]
 )
 
-if pagina == "Inicio":
-    inicio.mostrar_pagina()
-elif pagina == "Predicción meteorológica":
+st.markdown("<h3 style='text-align: center;'>Universidade de Santiago de Compostela</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Grao en Intelixencia Artificial</h3>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Proxecto Integrador I</h4>", unsafe_allow_html=True)
+
+st.write("Este proyecto se encuadra dentro del caso de Uso...")
+st.write("Se ha desarrollado esta aplicación web que...")
+
+if pagina == "Predicción meteorológica":
     prediccion.mostrar_pagina()
 elif pagina == "Herramienta de navegación (BETA)":
     herramienta_nav.mostrar_pagina()
