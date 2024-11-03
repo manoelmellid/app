2bdcaf04144708fdb01813a6dd3064f65024fe5f
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:07:35 +0100

    Update navegacion.py

--- pages/navegacion.py
+++ pages/navegacion.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import obtener_coordenadas as obcoor, pronostico as prn
 
-def show():
-    st.header("Herramienta de navegacion geospacial")
-    # Aquí incluirías todo el código específico para la predicción
+
+st.header("Herramienta de navegacion geospacial")
+# Aquí incluirías todo el código específico para la predicción
