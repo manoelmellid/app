77354df334780e78869266c40bc53d230f472bfa
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 13:50:13 +0100

    Update 3_flujos.py

--- pages/3_flujos.py
+++ pages/3_flujos.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import obtener_coordenadas as obcoor, pronostico as prn
 
 st.set_page_config(page_title="Modelo predictivo de flujos")
 
