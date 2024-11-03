c41841395daaadb898470e3684c2a7b81ed72a90
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:52:16 +0100

    Update and rename navegacion.py to 2_navegacion.py

--- pages/navegacion.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-
-st.header("Herramienta de navegacion geospacial")
-# Aquí incluirías todo el código específico para la predicción
