2f9e65bb26d795b7f97075a9100ab64f8cf2db10
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:53:10 +0100

    Update and rename prediccion.py to 1_prediccion.py

--- pages/1_prediccion.py
+++ pages/1_prediccion.py
@@ -4,6 +4,18 @@ import numpy as np
 
 from utils import obtener_coordenadas as obcoor, pronostico as prn
 
+st.set_page_config(page_title="Predicción meteorológica")
+
+col1, col2, col3 = st.columns([3,3,3])
+with col1:
+    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
+with col3:
+    st.header("SMETRIA")
+
+st.markdown("<h3 style='text-align: center;'>Sistema de monitorización de eventos en tramos</h3>", unsafe_allow_html=True)
+
+st.divider()
+
 st.header("Predicción meteorológica")
 longitud = None
 latitud = None
