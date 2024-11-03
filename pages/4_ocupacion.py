1bc331bf47f74780e0be42398979e7b236ab31db
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:52:47 +0100

    Update and rename ocupacion.py to 4_ocupacion.py

---
+++ pages/4_ocupacion.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Modelo predictivo de ocupación")
+
+col1, col2, col3 = st.columns([3,3,3])
+with col1:
+    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
+with col3:
+    st.header("SMETRIA")
+
+st.markdown("<h3 style='text-align: center;'>Sistema de monitorización de eventos en tramos</h3>", unsafe_allow_html=True)
+
+st.divider()
+
+st.header("Modelo predictivo de ocupacion")
+# Aquí incluirías todo el código específico para la predicción
--- pages/ocupacion.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.header("Modelo predictivo de ocupacion")
-# Aquí incluirías todo el código específico para la predicción
