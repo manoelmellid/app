b1e89d57afba7d2c84d24d8423648b1efba86e6e
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:07:49 +0100

    Update ocupacion.py

--- pages/ocupacion.py
+++ pages/ocupacion.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import obtener_coordenadas as obcoor, pronostico as prn
 
-def show():
-    st.header("Modelo predictivo de ocupacion")
-    # Aquí incluirías todo el código específico para la predicción
+st.header("Modelo predictivo de ocupacion")
+# Aquí incluirías todo el código específico para la predicción
