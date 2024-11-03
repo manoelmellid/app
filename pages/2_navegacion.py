187c92b74ddca23a26cf7ba7e37541d60bf5d270
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:53:57 +0100

    Update 2_navegacion.py

--- pages/2_navegacion.py
+++ pages/2_navegacion.py
@@ -10,8 +10,6 @@ with col1:
 with col3:
     st.header("SMETRIA")
 
-st.markdown("<h3 style='text-align: center;'>Sistema de monitorización de eventos en tramos</h3>", unsafe_allow_html=True)
-
 st.divider()
 
 st.header("Herramienta de navegacion geospacial")
