8effbdc67d92d12932f0b7846bfab612e2804edf
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 12:05:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pages import prediccion, navegacion, flujos, ocupacion
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="Inicio",
 )
 
 col1, col2, col3 = st.columns([3,3,3])
